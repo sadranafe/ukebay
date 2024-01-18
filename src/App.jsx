26594316b36fb11f9c25e4de5ed663eef2308676
mohammad sadra nafe <sadranafe7@gmail.com>
2024-01-18 23:29:46 +0330

    Routes of the project created and added to project in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProductsDetails from "./pages/ProductsDetails";
+import Cart from "./pages/cart";
+import ContactUs from "./pages/ContactUs";
+import Login from "./pages/Login";
+import Customers from "./pages/Customers";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <>
-      <h1>hello world</h1>
+        <Routes>
+            <Route index element = {<Home/>}/>
+            <Route path = "/products" element = {<Products/>}/>
+            <Route path = "/products/*" element = {<ProductsDetails/>}/>
+            <Route path = "/cart" element = {<Cart/>}/>
+            <Route path = "/contact-us" element = {<ContactUs />}/>
+            <Route path = "/login" element = {<Login/>}/>
+            <Route path = "/customers" element = {<Customers/>}/>
+            <Route path = "*" element = {<NotFound/>}/>
+        </Routes>
     </>
   );
 };
