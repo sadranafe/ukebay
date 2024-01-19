65a8ed0e122ed0f746b062bec516341b0f9d6fc4
mohammad sadra nafe <sadranafe7@gmail.com>
2024-01-19 16:55:55 +0330

    dummy_customers.js created and contains id , title and name

---
+++ src/DummyData/dummy_customers.js
@@ -0,0 +1,34 @@
+const Dummy_Customers = [
+    {
+        id : 1,
+        title : 'meta',
+        name : 'bx bxl-meta',
+    },
+    {
+        id : 2,
+        title : 'slack',
+        name : 'bx bxl-slack'
+    },
+    {
+        id : 3,
+        title : 'airbnb',
+        name : 'bx bxl-airbnb'
+    },
+    {
+        id : 4,
+        title : 'tiktok',
+        name : 'bx bxl-tiktok'
+    },
+    {
+        id : 5,
+        title : 'dribbble' ,
+        name : 'bx bxl-dribbble'
+    },
+    {
+        id : 6,
+        title : 'twitch',
+        name : 'bx bxl-twitch'
+    },
+]
+                    
+export default Dummy_Customers;
