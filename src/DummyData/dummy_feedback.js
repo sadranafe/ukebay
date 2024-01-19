75e07b5b30a4a2c5fc86274fcc68ce9d3686a19e
mohammad sadra nafe <sadranafe7@gmail.com>
2024-01-19 16:30:04 +0330

    DummyFeedback for feedback component created and contains id , img , name , position and description

---
+++ src/DummyData/dummy_feedback.js
@@ -0,0 +1,36 @@
+const DummyFeedback = [
+  {
+    id: 1,
+    img: "https://wicks-store-demo.myshopify.com/cdn/shop/files/people1.png?v=1642839298",
+    name: "destinee",
+    position: "customer",
+    description:
+      "Saved our business! We have no regrets! Thanks for the great service. Wicks is worth much more than I paid Saved our business! We have no regrets! Thanks for the great service. Wicks is worth much more!",
+  },
+  {
+    id: 2,
+    img: "https://wicks-store-demo.myshopify.com/cdn/shop/files/people4.png?v=1642839298",
+    name: "tony",
+    position: "customer",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nesciunt tempora voluptatum deleniti error. Voluptates ad sint vitae earum deleniti. Iure soluta velit nesciunt, sit at quibusdam. Saepe in eum nihil necessitatibus provident sint laboriosam enim molestiae.",
+  },
+  {
+    id: 3,
+    img: "https://wicks-store-demo.myshopify.com/cdn/shop/files/people3.png?v=1642839298",
+    name: "anthony",
+    position: "marketing personal",
+    description:
+      "nesciunt tempora voluptatum deleniti error. Voluptates ad sint vitae an I paid Saved our business! We have no regrets! Thanks for the great service. Wicks is worth much more nesciunt tempora iti. Iure soluta velit nesciunt, sit at quibu. Voluptates ad sint vitae !",
+  },
+  {
+    id: 4,
+    img: "https://wicks-store-demo.myshopify.com/cdn/shop/files/people2.png?v=1642839297",
+    name: "anna",
+    position: "marketing personal",
+    description:
+      "iti. Iure soluta velit nesciunt, sit at quibuts! Thanks for the great service. Wicks is worth much more than I paid Saved our business! We have no regrets! Thanks for the iti. Iure soluta velit nesciunt, sit at quibuworth muchiti. Iure soluta iti. Iure soluta velit nesciun!",
+  },
+];
+
+export default DummyFeedback;
