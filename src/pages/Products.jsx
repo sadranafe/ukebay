6faa9e8cb62d10d26367f92e849e8371a2a7b00a
mohammad sadra nafe <sadranafe7@gmail.com>
2024-01-19 18:48:45 +0330

    data got with axios from fakeStoreApi in products.jsx and renders the product.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import { useState , useEffect } from "react";
+import axios from "axios";
+import Product from "../components/Product";
 
 const Products = () => {
+    const [DUMMYDATA, setDUMMYDATA] = useState([]);
+
+    useEffect(() => {
+        axios.get('/products')
+            .then(res => setDUMMYDATA(res));
+    }, [])
+
+
     return (
-        <div>
-            products
-        </div>
+        <>
+            <div className = "my-7 flex flex-wrap justify-center items-start">
+                <div className = "w-full flex flex-wrap justify-center items-center mb-3">
+                    search  & sort
+                </div>
+
+
+                <div className = {`w-10/12 flex flex-wrap justify-center items-stretch ${DUMMYDATA.length === 0 && 'h-[67vh]'}`}>
+                    {
+                        DUMMYDATA.length === 0 
+                        ?
+                        <p>loading...</p>
+                        :
+                        DUMMYDATA.map((item, index) =>
+                            <Product key = {index} data = {item} trendingMode = {false} />
+                        )
+                    }
+                </div>
+            </div>
+        </>
     );
 };
 
