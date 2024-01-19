3f3e3349bc298d5b684a3eeba1e17a348c3c3f8e
mohammad sadra nafe <sadranafe7@gmail.com>
2024-01-19 15:48:17 +0330

    axios configed in Main.jsx. the config contains baseURL and response interceptors

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://fakestoreapi.com'
+axios.interceptors.response.use(res => res.data )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
