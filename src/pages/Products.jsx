a7371374da603f3e4b0593ce6e06c23605a3b4ec
mohammad sadra nafe <sadranafe7@gmail.com>
2024-01-21 23:28:13 +0330

    if the filteredProducts is empty, show some text and if it is not empty, show the filteredProducts.

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -48,10 +48,14 @@ const Products = () => {
 
                 <div className = {`w-10/12 flex flex-wrap justify-center items-stretch ${DUMMYDATA.length === 0 && 'h-[67vh]'}`}>
                     {
-                        DUMMYDATA.length === 0 
-                        ?
-                        <Loader/>
-                        :
+                        DUMMYDATA.length === 0 ?
+                        <Loader/> :
+                        filteredProducts.length === 0 ? 
+
+                        <div className = "w-full bg-emer ald-300 flex flex-wrap select-none justify-center items-center mt-10">
+                            <p className = "capitalize text-xl">nothing found ! 🥹</p>
+                        </div> : 
+                        
                         filteredProducts.map((item, index) =>
                             <Product key = {index} data = {item} trendingMode = {false} />
                         )
