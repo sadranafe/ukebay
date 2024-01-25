4e6b84082b4a9bc74e234dc7b87c43180c085bc5
mohammad sadra nafe <sadranafe7@gmail.com>
2024-01-26 00:57:12 +0330

    add skeleton loader from MUI to the trendingProducts.jsx

--- src/components/TrendingProducts.jsx
+++ src/components/TrendingProducts.jsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@mui/material";
 import UseProducts from "../hooks/useProducts";
 import Product from "./Product";
 
@@ -17,6 +18,13 @@ const TrendingProducts = () => {
 
                 <div className = "w-full flex flex-wrap justify-center items-stretch p-5">
                     {
+                        DUMMYDATA.length === 0 ? 
+                        <div className = "p-5 border rounded-xl">
+                            <Skeleton animation = 'wave' variant = "rectangular" width = {200} height = {200} style = {{borderRadius : '12px'}} />
+                            <Skeleton animation = 'wave' variant = "text" style = {{borderRadius : '6px' , marginTop : '7px'}} />
+                            <Skeleton animation = 'wave' variant = "text" style = {{borderRadius : '6px' , marginTop : '2px'}} />
+                            <Skeleton animation = 'wave' variant = "text" style = {{borderRadius : '6px' , marginTop : '-5px' , padding : '20px'}} />
+                        </div> :
                         filteredTrendingProducts.map((item , index) => (
                             <Product key = {index} data = {item} trendingMode = {true}/>
                         ))
